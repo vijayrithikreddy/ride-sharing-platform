ae738cb39a82d968642c83fcb02a59fe559bffd4
vijayrithikreddy <vijayrithikreddy@gmail.com>
2026-07-21 15:34:04 +0530

    feat: configured and initialized Eureka Server

---
+++ eurekaservice/src/test/java/com/rideshare/eurekaservice/EurekaserviceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.rideshare.eurekaservice;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EurekaserviceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
