4c58429940e5893b0c84cfd4b5a36684164236d9
vijayrithikreddy <vijayrithikreddy@gmail.com>
2026-07-27 21:57:16 +0530

    feat: implemented ride matching service by decoding polylines

---
+++ rideservice/src/main/java/com/rideshare/rideservice/dto/UserSummaryDto.java
@@ -0,0 +1,25 @@
+package com.rideshare.rideservice.dto;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+import java.util.UUID;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+@Builder
+public class UserSummaryDto {
+
+    private UUID authUserId;
+
+    private String firstName;
+
+    private String lastName;
+
+    private String profilePictureUrl;
+
+    private VehicleSummaryDto vehicle;
+}
