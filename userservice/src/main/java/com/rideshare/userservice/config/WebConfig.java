7fdbd189567d44c712452460ce36db926eea6dab
vijayrithikreddy <vijayrithikreddy@gmail.com>
2026-08-11 08:49:11 +0530

    deployment changes

--- userservice/src/main/java/com/rideshare/userservice/config/WebConfig.java
+++ userservice/src/main/java/com/rideshare/userservice/config/WebConfig.java
@@ -1,5 +1,6 @@
 package com.rideshare.userservice.config;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
@@ -7,13 +8,16 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 @Configuration
 public class WebConfig implements WebMvcConfigurer {
 
+    @Value("${app.upload.profile-picture-path}")
+    private String profilePicturePath;
+
     @Override
     public void addResourceHandlers(ResourceHandlerRegistry registry) {
 
         registry
                 .addResourceHandler("/uploads/profile-pictures/**")
                 .addResourceLocations(
-                        "file:/app/uploads/profile-pictures/"
+                        "file:" + profilePicturePath + "/"
                 );
     }
 }
